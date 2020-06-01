98fe865d5e2015b1e8a50ce32da3b24520ee176c
YaCarl <41334639+YaCarl@users.noreply.github.com>
2020-06-01 03:01:31 +0300

    Update Econometrics.R

--- Econometrics.R
+++ Econometrics.R
@@ -1,6 +1,6 @@
-library("xlsx") #text2
+library("xlsx")
 
-###ALTMAN Z-SCORE MODEL SCORES & TAFFLER'S MODEL SCORES:
+###ALTMAN Z-SCORE MODEL SCORES & TAFFLER'S MODEL SCORES for bankrupt companies:
 
 setwd("/Users/irina/Desktop/companies_sample_2/bankrupt")
 bankrupt_сomp <- list.files(pattern = "\\.xlsx$")
@@ -9,6 +9,8 @@ Z_bankrupt_1y <- c()
 T_bankrupt_2y <- c()
 T_bankrupt_1y <- c()
 for (i in bankrupt_сomp) {
+  
+#Raw data on balance sheet and income statement
   data_BS<-read.xlsx(file.path(i),
                                "Balance sheet", 
                                rowIndex = 15:59, 
@@ -20,6 +22,7 @@ for (i in bankrupt_сomp) {
                    colIndex = 3:7, 
                    colNames=TRUE,rowNames=TRUE,stringsAsFactors=FALSE)
   
+#Preparation of the data
   data_BS <- data_BS[!data_BS$Code=="", ]
   data_BS <- data_BS[!is.na(data_BS$Code), ]
   data_BS_cl <- data_BS[-c(1,3,5,6,7)]
@@ -37,6 +40,7 @@ for (i in bankrupt_сomp) {
   rownames(data_IS_cl) <- data_IS[,1]
   if (ncol(data_IS_cl)==2) {colnames(data_IS_cl) <- c(1,2)} else {colnames(data_IS_cl <- c(1))}
 
+#Calculation of Z-scores for years 1 and 2:                                 
   Z_score_1y <- 3.25+6.56*(data_BS_cl["1200",1]-data_BS_cl["1500",1])/data_BS_cl["1600",1]+3.26*data_BS_cl["1370",1]/data_BS_cl["1600",1]+6.72*data_IS_cl["2300",1]/data_BS_cl["1600",1]+1.05*data_BS_cl["1300",1]/(data_BS_cl["1400",1]+data_BS_cl["1500",1])
   Z_score_2y <- 3.25+6.56*(data_BS_cl["1200",2]-data_BS_cl["1500",2])/data_BS_cl["1600",2]+3.26*data_BS_cl["1370",2]/data_BS_cl["1600",2]+6.72*data_IS_cl["2300",2]/data_BS_cl["1600",2]+1.05*data_BS_cl["1300",2]/(data_BS_cl["1400",2]+data_BS_cl["1500",2])
   T_score_1y <- 0.53*data_IS_cl["2300",1]/data_BS_cl["1500",1]+0.13*data_BS_cl["1200",1]/data_BS_cl["1700",1]+0.18*data_BS_cl["1500",1]/data_BS_cl["1600",1]+0.16*data_IS_cl["2110",1]/data_BS_cl["1600",1]
@@ -46,10 +50,13 @@ for (i in bankrupt_сomp) {
   Z_bankrupt_1y <- c(Z_bankrupt_1y,Z_score_1y)
   T_bankrupt_2y <- c(T_bankrupt_2y,T_score_2y)
   T_bankrupt_1y <- c(T_bankrupt_1y,T_score_1y)}
+                                  
+#Extra cleaning specifically for this sample of companies since some of the files had different formatting
 T_bankrupt_1y <- na.omit(T_bankrupt_1y)
 
 
-
+###ALTMAN Z-SCORE MODEL SCORES & TAFFLER'S MODEL SCORES for non-bankrupt companies:
+#The structure of the following code is the same as of the previous one:
 setwd("/Users/irina/Desktop/companies_sample_2/nonbankrupt")
 nonbankrupt_сomp <- list.files(pattern = "\\.xlsx$")
 Z_nonbankrupt_2y <- c()
